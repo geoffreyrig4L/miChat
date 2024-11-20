f3094999ae3b24b0bfbbad282ac2272a6f9daa23
geoffreyrig4l <rigalgeoffrey29@gmail.com>
2024-11-20 15:23:21 +0100

    add form + angular material

--- frontend/src/app/features/docs/docs.component.ts
+++ frontend/src/app/features/docs/docs.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'docs',
   templateUrl: './docs.component.html',
-  styleUrl: './docs.component.css',
 })
 export class DocsComponent {}
--- frontend/src/app/features/sign-in/sign-in.component.ts
+++ frontend/src/app/features/sign-in/sign-in.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'sign-in',
+  imports: [
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {}
+export class SignInComponent {
+  username = new FormControl('');
+  password = new FormControl('');
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log('username:', this.username.value);
+    console.log('password:', this.password.value);
+  }
+}
