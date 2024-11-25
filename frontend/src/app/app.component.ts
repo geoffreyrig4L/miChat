61bcc1afe26d64fa29efb3e8fcf0a2e71bfce4d5
Romain Bidault <romain.bidault@supdevinci-edu.fr>
2024-11-25 15:44:20 +0100

    secure and add sign-in in interface

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { SignInComponent } from './features/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatButtonModule, ListComponent],
+  imports: [RouterOutlet, MatButtonModule, ListComponent, SignInComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'miChat';
+
+  isAuth: boolean = false;
 }
