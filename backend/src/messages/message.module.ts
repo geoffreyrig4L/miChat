104ef4c738508f8b4318abb015dc3d2d38ee8f31
geoffreyrig4l <rigalgeoffrey29@gmail.com>
2024-11-30 18:38:19 +0100

    join message et conv mais ça galette

--- backend/src/messages/message.module.ts
+++ backend/src/messages/message.module.ts
@@ -4,12 +4,17 @@ import { Message, MessageSchema } from '@src/schemas/Message.schema';
 import { MessageController } from './message.controller';
 import { MessageService } from './message.service';
 import { User, UserSchema } from '@src/schemas/User.schema';
+import {
+  Conversation,
+  ConversationSchema,
+} from '@src/schemas/Conversation.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Message.name, schema: MessageSchema },
       { name: User.name, schema: UserSchema },
+      { name: Conversation.name, schema: ConversationSchema },
     ]),
   ],
   controllers: [MessageController],
